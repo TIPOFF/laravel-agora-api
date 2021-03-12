bc27154aaea4669b2b77de87d9b0979fa16a4096
Kyle Barney <kyle@kylebarney.dev>
2021-03-12 08:54:50 -0700

    Scaffold AgoraController and configure route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Tipoff\LaravelAgoraApi\Http\Controllers\AgoraController;
 
-Route::post('/agora/retrieve-token', 'App\Http\Controllers\AgoraController@retrieveToken');
+Route::post('/agora/retrieve-token', [AgoraController::class, 'retrieveToken'])->name('agora.retrieve-token');
---
+++ src/Http/AgoraController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tipoff\LaravelAgoraApi\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AgoraController extends Controller
+{
+    public function retrieveToken(Request $request) {
+        
+    }
+}
