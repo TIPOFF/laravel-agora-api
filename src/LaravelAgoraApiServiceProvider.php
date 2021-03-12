6ffb316cf50db34c2e487e1167a33a843b1c4612
Kyle Barney <kyle@kylebarney.dev>
2021-03-12 09:06:01 -0700

    Allow custom middleware on routes

--- src/LaravelAgoraApiServiceProvider.php
+++ src/LaravelAgoraApiServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tipoff\LaravelAgoraApi;
 
+use Illuminate\Support\Facades\Route;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -13,9 +14,22 @@ class LaravelAgoraApiServiceProvider extends TipoffServiceProvider
     {
         $package
             ->name('agora')
-            ->hasViews()
             ->hasConfigFile()
             ->hasRoute('routes/web.php');
+    }
+
+    public function boot()
+    {
+        Route::group($this->routeConfiguration(), function () {
+            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        });
+    }
+
+    protected function routeConfiguration()
+    {
+        return [
+            'middleware' => config('agora.middleware'),
+        ];
     }
 
     public function register()
