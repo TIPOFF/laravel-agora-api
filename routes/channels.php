1ef7bc84e3761d44886ff1a9df1f2c9d75afe46d
Kyle Barney <kyle@kylebarney.dev>
2021-03-13 16:33:26 -0700

    Add permissions check + migration for permission

--- routes/channels.php
+++ routes/channels.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel(config('agora.channel_name'), function ($user) {
-    if (Auth::check()) {
+    if (Auth::check() && $user->hasPermissionTo('make video call')) {
         return [
             'id' => $user->id,
             'name' => $user->name
