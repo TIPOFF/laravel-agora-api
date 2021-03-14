0a900c59e705835087dd6b0086fbec6a62667a54
Kyle Barney <kyle@kylebarney.dev>
2021-03-13 22:36:35 -0700

    Test for placing call method - successful

--- tests/Feature/Http/AgoraControllerTest.php
+++ tests/Feature/Http/AgoraControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tipoff\LaravelAgoraApi\Tests\Feature\Http;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Event;
 use Tipoff\Authorization\Models\User;
 use Tipoff\LaravelAgoraApi\Events\DispatchAgoraCall;
@@ -11,6 +12,7 @@ use Tipoff\LaravelAgoraApi\Tests\TestCase;
 class AgoraControllerTest extends TestCase
 {
     use DatabaseTransactions;
+    use WithFaker;
 
     public function testUnauthenticatedUsersCannotRetrieveAToken()
     {
@@ -56,5 +58,20 @@ class AgoraControllerTest extends TestCase
             ->postJson(route('agora.place-call'));
 
         $response->assertStatus(422);
+    }
+
+    public function testAuthorizedUsersCanPlaceACall()
+    {
+        Event::fake();
+
+        $response = $this->actingAs(self::createPermissionedUser('make video call', true))
+            ->postJson(route('agora.place-call'), [
+                'channel_name' => $this->faker->word,
+                'recipient_id' => User::factory()->create()->id
+            ]);
+
+        Event::assertDispatched(DispatchAgoraCall::class);
+
+        $response->assertStatus(200);
     }
 }
