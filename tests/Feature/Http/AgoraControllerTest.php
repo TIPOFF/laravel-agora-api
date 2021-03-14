71a3e676af2b14e9849b87d8eb34b2efbbd39e64
Kyle Barney <kyle@kylebarney.dev>
2021-03-13 22:32:27 -0700

    Add data validation tests

--- tests/Feature/Http/AgoraControllerTest.php
+++ tests/Feature/Http/AgoraControllerTest.php
@@ -3,7 +3,9 @@
 namespace Tipoff\LaravelAgoraApi\Tests\Feature\Http;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Event;
 use Tipoff\Authorization\Models\User;
+use Tipoff\LaravelAgoraApi\Events\DispatchAgoraCall;
 use Tipoff\LaravelAgoraApi\Tests\TestCase;
 
 class AgoraControllerTest extends TestCase
@@ -38,5 +40,21 @@ class AgoraControllerTest extends TestCase
             ->postJson(route('agora.place-call'));
 
         $response->assertStatus(403);
+    }
+
+    public function testInvalidRequestsDoNotReturnAToken()
+    {
+        $response = $this->actingAs(self::createPermissionedUser('make video call', true))
+            ->postJson(route('agora.retrieve-token'));
+
+        $response->assertStatus(422);
+    }
+
+    public function testInvalidRequestsDoNotReturnPlaceACall()
+    {
+        $response = $this->actingAs(self::createPermissionedUser('make video call', true))
+            ->postJson(route('agora.place-call'));
+
+        $response->assertStatus(422);
     }
 }
