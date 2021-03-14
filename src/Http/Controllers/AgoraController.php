0d0150045f2b24066fe0056435c90f00f5f2e9bf
Kyle Barney <kyle@kylebarney.dev>
2021-03-13 17:27:03 -0700

    Create token retrieval call

--- src/Http/Controllers/AgoraController.php
+++ src/Http/Controllers/AgoraController.php
@@ -3,11 +3,20 @@
 namespace Tipoff\LaravelAgoraApi\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Tipoff\LaravelAgoraApi\AgoraDynamicKey\RtcTokenBuilder;
 
 class AgoraController extends Controller
 {
     public function retrieveToken(Request $request)
     {
-        return true;
+        return RtcTokenBuilder::buildTokenWithUserAccount(
+            config('agora.credentials.app_id'),
+            config('agora.credentials.certificate'),
+            $request->channelName,
+            Auth::user()->name,
+            RtcTokenBuilder::RoleAttendee,
+            now()->getTimestamp() + 3600
+        );
     }
 }
