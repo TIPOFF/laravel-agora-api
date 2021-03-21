88f5937a4fa7ff39526e547898ab62429650430d
Kyle Barney <kyle@kylebarney.dev>
2021-03-20 23:06:27 -0600

    Start Vue templates

--- resources/js/components/AgoraIncomingCallAlert.vue
+++ resources/js/components/AgoraIncomingCallAlert.vue
@@ -1,5 +1,19 @@
 <template>
-    
+    <div class="incoming-call-alert" v-if="callIncoming">
+        <div>
+            <p>Incoming call from: {{ incomingCaller }}</p>
+
+            <div class="btn-group" role="group">
+                <button @click="acceptCall">
+                    Accept
+                </button>
+
+                <button @click="declineCall">
+                    Decline
+                </button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
--- resources/js/components/AgoraVideoDisplay.vue
+++ resources/js/components/AgoraVideoDisplay.vue
@@ -1,5 +1,22 @@
 <template>
-    
+    <div id="video-container" v-if="connected">
+        <div id="local-video"></div>
+        <div id="remote-video"></div>
+
+        <div class="call-actions">
+            <button type="button" @click="toggleAudio">
+                {{ sendAudio ? "Mute" : "Unmute" }}
+            </button>
+
+            <button type="button" @click="toggleVideo">
+                {{ sendVideo ? "Hide Video" : "Show Video" }}
+            </button>
+
+            <button type="button" @click="endCall">
+                End Call
+            </button>
+        </div>
+    </div>
 </template>
 
 <script>
